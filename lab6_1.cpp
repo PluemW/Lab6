04bd8e0ccec8ea7cafbdcd3e4f86f63159e49a91
PluemW <wittawat_jar@cmu.ac.th>
2022-12-09 16:20:38 +0700

    Filled out

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,24 @@
 using namespace std;
 
 int main(){
+    int x;
+    int e = 0;
+    int o = 0;
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> x;
+    while(x!=0){
+        if(x%2==0){
+            e++;
+            cout << "Enter an integer: ";
+            cin >> x;
+        }
+        else{
+            cout << "Enter an integer: ";
+            cin >> x;
+            o++;
+        }
+    }
+    cout << "#Even numbers = " << e << "\n";
+    cout << "#Odd numbers = " << o << "\n";
     return 0;
 }
--- lab6_2.cpp
+++ lab6_2.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
-
+#include <cmath>
 using namespace std;
+
+double deg2rad(double x){
+    x = x*(M_PI/180);
+    return x;
+}
+double rad2deg(double x){
+    x = x*(180/M_PI);
+    return x;
+}
+double findXComponent(double v1, double v2, double x1, double x2){
+    double xcomp;
+    xcomp = (v1*(cos(x1))) + (v2*(cos(x2)));
+    return xcomp; 
+}
+double findYComponent(double v1, double v2, double y1, double y2){
+    double ycomp;
+    ycomp = (v1*(sin(y1))) + (v2*(sin(y2)));
+    return ycomp; 
+}
+double pythagoras(double xcomp, double ycomp){
+    double f;
+    f = pow((pow(xcomp,2)+pow(ycomp,2)),0.5);
+    return f;
+}
+double showResult(double f, double deg){
+    cout << "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%" << endl;
+    cout << "Length of the resultant vector = " << f << endl;
+    cout << "Direction of the resultant vector (deg) = " << deg << endl;
+    cout << "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%" << endl;
+    return 0;
+}
 
 int main(){
     double l1,l2,a1,a2,xcomp,ycomp,result_vec_length,result_vec_direction;
@@ -21,4 +52,5 @@ int main(){
     result_vec_direction = rad2deg(atan2(ycomp,xcomp)); 
 
     showResult(result_vec_length,result_vec_direction);
+    return 0;
 }
